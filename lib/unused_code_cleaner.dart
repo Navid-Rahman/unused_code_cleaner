fddb15528ae72bc552e20165ebb6c92d312e4a7f
navid <navidrahman92@gmail.com>
2025-07-12 16:27:46 +0600

    Add main package file with exports

--- lib/unused_code_cleaner.dart
+++ lib/unused_code_cleaner.dart
@@ -1,5 +1,5 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library unused_code_cleaner;
+
+export 'src/models/analysis_result.dart';
+export 'src/models/unused_item.dart';
+export 'src/models/cleanup_options.dart';
